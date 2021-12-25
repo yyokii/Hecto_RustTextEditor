ec4a1083102d55472c8e624c3acfdd31a0f75c6f
yyokii <yyokii.h@gmail.com>
2021-12-25 22:09:18 +0900

    ✨ Create a text viewer

---
+++ memo.md
@@ -0,0 +1,2 @@
+* terminalの開始が1なのを0indexに変更したんやっけ？
+* strとStringの違いが気になる
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 
+mod document;
 mod editor;
+mod row;
 mod terminal;
+use document::Document;
 use editor::Editor;
-use terminal::Terminal;
 use editor::Position;
+use row::Row;
+use terminal::Terminal;
 
 fn main() {
     Editor::default().run();
